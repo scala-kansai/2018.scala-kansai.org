3658bd2b7d1e414606b702f16151b1fbb434ece7
yasui05821 <yasui05821@gmail.com>
2018-05-28 19:01:07 +0900

    update

--- components/pages/Top/Access.vue
+++ components/pages/Top/Access.vue
@@ -5,6 +5,41 @@
       <div class="p-access_read">
         Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）
       </div>
+      <div class="p-access_list">
+        <div class="p-access_listItem">
+          <div class="map">マップが入る</div>
+          <div class="detail">
+            <div class="title">
+              <span class="day">1日目</span>
+              天満研修センター
+            </div>
+            <p>〒530-0034　大阪市北区錦町2-21</p>
+            <ul class="list">
+              <li>JR「天満駅」より徒歩2分</li>
+              <li>地下鉄堺筋線「扇町駅」 1番出口 より徒歩７分</li>
+              <li>地下鉄谷町線「天神橋筋六丁目駅」 12番出口より徒歩10分</li>
+            </ul>
+            <a href="#" class="link" target="_blank">Googleマップで開く</a>
+          </div>
+        </div>
+        <div class="p-access_listItem">
+          <div class="map">マップが入る</div>
+          <div class="detail">
+            <div class="title">
+              <span class="day">2日目</span>
+              さくらインターネット株式会社 本社
+            </div>
+            <p>〒530-0011 大阪市北区大深町4番20号 グランフロント大阪タワーA 35階</p>
+            <ul class="list">
+              <li>JR「大阪駅」より徒歩7分</li>
+              <li>地下鉄御堂筋線「梅田駅」より徒歩9分</li>
+              <li>阪神「梅田駅」、阪急「梅田駅」より徒歩10分</li>
+              <li>地下鉄四つ橋線「西梅田駅」、地下鉄谷町線「東梅田駅」、JR「北新地駅」より徒歩13分</li>
+            </ul>
+            <a href="#" class="link" target="_blank">Googleマップで開く</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -33,6 +68,43 @@
     &_read {
       @include secRead;
     }
-    
+
+    &_list {
+
+    }
+
+    &_listItem {
+      margin-bottom: 60px;
+      text-align: left;
+
+      .map {
+        background: red;
+      }
+
+      .detail {
+        padding: 20px 40px 40px;
+        background: white;
+      }
+
+      .title {
+        font-size: 2rem;
+        font-weight: bold;
+      }
+      .day {
+        margin-right: .5em;
+        font-weight: normal;
+        font-size: 2rem;
+      }
+      .list {
+        margin-bottom: 2em;
+      }
+
+      .link {
+        font-size: 1.6rem;
+        padding-bottom: .5em;
+        border-bottom: 1px solid $clr_accent;
+        color: $clr_accent;
+      }
+    }
   }
 </style>
--- components/pages/Top/Wanted.vue
+++ components/pages/Top/Wanted.vue
@@ -5,6 +5,32 @@
       <div class="p-wanted_read">
         Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）
       </div>
+      <div class="p-wanted_list row">
+        <div class="col">
+          <div class="p-wanted_listItem">
+            <div class="title">
+              <i class="fas fa-microphone"></i>
+              スピーカー募集</div>
+            <div class="description">Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）</div>
+            <div class="deadline">応募締切：8月31日（仮）</div>
+            <a class="btn" href="#" target="_blank">
+              <i class="fas fa-microphone"></i>
+              スピーカー申込み</a>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-wanted_listItem">
+            <div class="title">
+              <i class="fas fa-star"></i>
+              スポンサー募集</div>
+            <div class="description">Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）</div>
+            <div class="deadline">応募締切：8月31日（仮）</div>
+            <a class="btn" href="#" target="_blank">
+              <i class="fas fa-star"></i>
+              スポンサー申込み</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -26,6 +52,10 @@
     @include secPadding;
     background: $clr_bg-lightOrange;
 
+    .btn {
+      @include btnIcon();
+    }
+
     &_heading {
       @include secHeading;
     }
@@ -33,6 +63,32 @@
     &_read {
       @include secRead;
     }
+
+    &_listItem {
+      padding: 40px;
+      border-radius: 10px;
+      background: white;
+
+      .title {
+        margin-bottom: .5em;
+        font-size: 2.5rem;
+        font-weight: bold;
+        i {
+          margin-right: .2em;
+          color: $clr_brown;
+        }
+      }
+      .description {
+        margin-bottom: 2em;
+      }
+      .deadline {
+        font-size: 1.6rem;
+        font-weight: bold;
+        margin-bottom: 1.5em;
+      }
+
+    }
+
 
   }
 </style>
