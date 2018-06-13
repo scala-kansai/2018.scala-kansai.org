995e4daa71b780e2df6366b2d113de663a1f4f12
yasui05821 <yasui05821@gmail.com>
2018-06-14 00:43:09 +0900

    求人情報ページ　作成
    # Conflicts:
    #	components/pages/Top/About.vue

--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,37 +1,515 @@
 <template>
-  <section class="container">
+  <section>
     <div>
-      <app-logo/>
-      <h1 class="title">
-        scala
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+      <div class="p-job_header">
+        <img src="~assets/images/logo.svg" class="p-job_logo" alt="Scala Kansai summit2018">
+      </div>
+      <div class="p-job">
+        <div class="c-container">
+          <h3 class="p-job_heading">
+            求人・広告
+          </h3>
+          <div class="p-job_read">
+            Scala関西Summit はScala初心者から上級者まで楽しめる、Scalaの技術を共有するイベントです（このテキストは仮で入れています。）
+          </div>
+          <div class="p-job_platinum">
+            <div class="title">
+              Platinum
+            </div>
+            <div>
+              <div class="list">
+                <div class="listItem">
+                  <div class="rank">
+                  </div>
+                  <div class="rankText">
+                    <p>Platinum</p>
+                    <i class="fa fa-star"></i>
+                  </div>
+                  <div class="inner">
+                    <a href="#" class="company">会社名</a>
+                    <span class="subtitle">会社のキャッチコピーなど</span>
+                    <a href="#" class="logoLink" ><div class="logo"/></a>
+                    <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                    <div class="linkArea">
+                      <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-job_gold">
+            <div class="title">
+              Gold
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Gold</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <span class="subtitle">会社のキャッチコピーなど</span>
+                  <a href="#" class="logoLink" ><div class="logo"/></a>
+                  <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Gold</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <span class="subtitle">会社のキャッチコピーなど</span>
+                  <a href="#" class="logoLink"><div class="logo"/></a>
+                  <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Gold</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <span class="subtitle">会社のキャッチコピーなど</span>
+                  <a href="#" class="logoLink"><div class="logo"/></a>
+                  <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-job_silver">
+            <div class="title">
+              Silver
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Silver</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <a href="#" class="logoLink" ><div class="logo"/></a>
+                    </div>
+                    <div class="col-sm">
+                      <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                    </div>
+                  </div>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Silver</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <a href="#" class="logoLink" ><div class="logo"/></a>
+                    </div>
+                    <div class="col-sm">
+                      <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                    </div>
+                  </div>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="list">
+              <div class="listItem">
+                <div class="rank">
+                </div>
+                <div class="rankText">
+                  <p>Silver</p>
+                  <i class="fa fa-star"></i>
+                </div>
+                <div class="inner">
+                  <a href="#" class="company">会社名</a>
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <a href="#" class="logoLink" ><div class="logo"/></a>
+                    </div>
+                    <div class="col-sm">
+                      <p class="description">俺は今単にその落第院というのの上をもっななら。何しろ今が関係通りはまあその煩悶うですまでを入れから切らたには記憶なりないですば、いっそにもなっなけれませんた。安危があるたものもとうてい事実をちゃんとでたある。よし嘉納さんでぼんやり詞どう煩悶に応じなら学校その兄弟それかろかをにおいて大記憶たらたたでしょと、この時分はあなたか日数他よりならが、三宅さんのので学校の私にさぞ小評とあるて私教授が小講演に着ようにあたかもご誤解になっないなて、初めてどうしても影響に思いましていなものに当てるなけれた。</p>
+                    </div>
+                  </div>
+                  <div class="linkArea">
+                    <a href="" class="btn" target="_blank"><i class="fas fa-external-link-alt"></i>広告・求人ページ</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-job_wanted">
+            <p>スポンサー随時受付中です！</p>
+            <dev>
+              <a href="" target="_blank" class="p-job_btn-wanted">
+                <i class="fas fa-star"></i>
+                スポンサー申込み
+              </a>
+            </dev>
+            <div>
+              <router-link
+                to="/"
+                class="p-job_btn-top">
+                <i class="fas fa-chevron-left"></i>
+                TOPに戻る
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
-export default {
-  components: {
-    AppLogo
-  }
-}
 </script>
 
-<style>
+<style lang="scss" scoped>
+  @import "~/assets/scss/library/_variable.scss";
+  @import "~/assets/scss/library/_mixin.scss";
 
+  .p-job {
+    @include secPadding;
+    background: $clr_bg-orange;
+
+    &_header {
+      background: white;
+    }
+    &_logo {
+      padding: 10px;
+      width: 75%;
+      max-width: 150px;
+      @include desktop {
+        padding: 20px;
+        width: 60%;
+        max-width: 300px;
+      }
+    }
+
+    &_heading {
+      @include secHeading;
+    }
+
+    &_read {
+      @include secRead;
+    }
+
+    /* 共通クラス */
+
+    .title {
+      @include title-border;
+    }
+    .row {
+      justify-content: flex-start;
+    }
+
+
+    .list {
+      box-sizing: border-box;
+      text-align: center;
+    }
+
+    .listItem {
+      margin-bottom: 40px;
+      box-sizing: border-box;
+      padding: 40px 0 0;
+      text-align: center;
+      background: white;
+      position: relative;
+
+      @include desktop {
+        padding: 50px 0 0;
+      }
+
+      .inner {
+        width: 100%;
+        margin: 0 auto 30px;
+        padding: 0 20px;
+        display: inline-block;
+        text-align: left;
+        box-sizing: border-box;
+
+        @include desktop {
+          margin: 0 auto 60px;
+        }
+      }
+
+      .company {
+        display: block;
+        padding-bottom: .1em;
+        width: 100%;
+        text-align: left;
+        font-size: 2rem;
+        font-weight: bold;
+        color: $clr_brown;
+      }
+      .subtitle {
+        display: inline-block;
+        margin-bottom: 20px;
+        font-size: 1.4rem;
+        color: $clr_brown;
+      }
+
+      .rank {
+        border-top: 45px solid #5CC5B8;
+        border-bottom: 45px solid transparent;
+        border-left: 45px solid transparent;
+        border-right: 45px solid #5CC5B8;
+        position: absolute;
+        top :0;
+        right :0;
+
+        @include desktop {
+          border-top: 55px solid #5CC5B8;
+          border-bottom: 55px solid transparent;
+          border-left: 55px solid transparent;
+          border-right: 55px solid #5CC5B8;
+        }
+      }
+      .rankText {
+        display: inline-block;
+        color: white;
+        position: absolute;
+        letter-spacing: 1px;
+        top : 4px;
+        right: 4px;
+        text-align: center;
+        @include desktop {
+          top : 8px;
+          right :8px;
+        }
+
+        p {
+          margin: 0;
+          font-size: 1.1rem;
+          @include desktop {
+            font-size: 1.4rem;
+          }
+        }
+        i {
+          font-size: 1.4rem;
+          @include desktop {
+            font-size: 2rem;
+          }
+        }
+      }
+
+      .linkArea {
+        text-align: center;
+      }
+      .description {
+        margin-bottom: 40px;
+      }
+
+      .btn {
+        @include btnIcon;
+      }
+    }
+
+    &_platinum {
+      margin-bottom: 30px;
+      @include desktop {
+        margin-bottom: 60px;
+      }
+
+      @include desktop {
+
+      }
+      .row {
+        justify-content: center;
+      }
+      .listItem {
+        .inner {
+          max-width: 750px;
+        }
+        .logoLink {
+          display: inline-block;
+          width: 100%;
+          text-align: center;
+        }
+        .logo {
+          margin:  0 auto;
+          display: inline-block;
+          margin-bottom: 15px;
+          width: 100%;
+          max-width: 600px;
+          background-image: url("http://placehold.it/600x480");
+          background-repeat: no-repeat;
+          background-size: contain;
+          background-position: center;
+
+          &::before {
+            content: "";
+            display: block;
+            padding-top: 80%;
+          }
+        }
+
+        .description {
+          display: block;
+          width: 100%;
+          box-sizing: border-box;
+
+        }
+
+      }
+    }
+
+    &_gold {
+      margin-bottom: 30px;
+      @include desktop {
+        margin-bottom: 60px;
+      }
+
+      .list {
+
+      }
+      .listItem {
+        .rank {
+          border-top: 40px solid #F7B444;
+          border-bottom: 40px solid transparent;
+          border-left: 40px solid transparent;
+          border-right: 40px solid #F7B444;
+          @include desktop {
+            border-top: 50px solid #F7B444;
+            border-bottom: 50px solid transparent;
+            border-left: 50px solid transparent;
+            border-right: 50px solid #F7B444;
+          }
+        }
+        .inner {
+          max-width: 100%;
+          @include desktop {
+            max-width: 750px;
+          }
+        }
+        .logo {
+          display: block;
+          margin: 0 auto;
+          margin-bottom: 15px;
+          width: 100%;
+          max-width: 250px;
+          background-image: url("http://placehold.it/300x240");
+          background-repeat: no-repeat;
+          background-size: contain;
+          background-position: center;
+
+          @include desktop {
+            max-width: 350px;
+          }
+
+          &::before {
+            content: "";
+            display: block;
+            padding-top: 80%;
+          }
+        }
+      }
+    }
+
+    &_silver {
+      .listItem {
+        .rank {
+          border-top: 40px solid #BDB5A9;
+          border-bottom: 40px solid transparent;
+          border-left: 40px solid transparent;
+          border-right: 40px solid #BDB5A9;
+          @include desktop {
+            border-top: 50px solid #BDB5A9;
+            border-bottom: 50px solid transparent;
+            border-left: 50px solid transparent;
+            border-right: 50px solid #BDB5A9;
+          }
+        }
+        .inner {
+          max-width: 100%;
+          @include desktop {
+            max-width: 750px;
+          }
+        }
+        .logo {
+          display: block;
+          margin: 0 auto;
+          width: 100%;
+          max-width: 160px;
+          background-image: url("http://placehold.it/200x160");
+          background-repeat: no-repeat;
+          background-size: contain;
+          background-position: center;
+          @include desktop {
+            margin: 0;
+            background-position: left;
+          }
+
+          @include desktop {
+            max-width: 200px;
+          }
+
+          &::before {
+            content: "";
+            display: block;
+            padding-top: 80%;
+          }
+        }
+        .description {
+          @include desktop {
+            margin-top: 0;
+          }
+        }
+
+      }
+    }
+    &_wanted {
+      margin-top: 40px;
+      font-size: 1.8rem;
+    }
+    &_btn-wanted {
+      @include btnIcon();
+      margin-bottom: 50px;
+
+    }
+    &_btn-top {
+      @include btnIcon(white,$clr_brown);
+    }
+  }
 </style>
