876b2eac801121b1b0c9169e1e1028732e0d8e4b
yasui05821 <yasui05821@gmail.com>
2018-08-31 13:27:37 +0900

    issue #26 スピーカー募集フォーム削除

--- components/pages/Top/Wanted.vue
+++ components/pages/Top/Wanted.vue
@@ -3,32 +3,10 @@
     <img src="~assets/images/leaf5.svg" class="leaf5">
     <img src="~assets/images/leaf7.svg" class="leaf7">
     <div class="c-container">
-      <h3 class="p-wanted_heading">スピーカー &amp; スポンサー募集</h3>
-      <div class="p-wanted_read">
-        Scala関西Summitは、Scalaにかかわるすべての人たちと一緒につくりあげていくイベントです。
-        イベント当日のコンテンツや、Scalaを利用されている企業のご支援など、ぜひご協力をお願いします。
-      </div>
-      <div class="p-wanted_list row">
-        <div class="col-md">
+      <h3 class="p-wanted_heading">スポンサー募集</h3>
+      <div class="p-wanted_list ">
+        <div>
           <div class="p-wanted_listItem">
-            <div class="title">
-              <i class="fas fa-microphone"></i>
-              スピーカー募集</div>
-            <div class="description">
-              Scala関西Summitでは、Scalaについて話をしたいすべての人に登壇のチャンスがあります！
-              日々のお仕事や、研究・学習の成果をぜひわたしたちに聴かせてください。
-            </div>
-            <div class="deadline">応募締切：2018年8月31日</div>
-            <a class="btn" href="https://docs.google.com/forms/d/e/1FAIpQLSer9U30kuLodBr_7x6j2R7c0IxHfLLyiJ6f4Pe-SJWyz3GUuw/viewform?usp=sf_link" target="_blank">
-              <i class="fas fa-microphone"></i>
-              スピーカー申込み</a>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="p-wanted_listItem">
-            <div class="title">
-              <i class="fas fa-star"></i>
-              スポンサー募集</div>
             <div class="description">
               Scala関西Summitでは、イベントを支援してくださるスポンサーを募集します。
               関西だけでなく、毎年日本全国から多く参加者が集まるこのイベントで、参加者とスポンサーの良い出会いがあるものと期待しています。
@@ -67,6 +45,7 @@
     @include secPadding;
     background: $clr_bg-lightOrange;
     position: relative;
+    text-align: center;
 
     .leaf5 {
       @include leaf;
@@ -109,14 +88,17 @@
     }
 
     &_listItem {
+      display: inline-block;
       margin-bottom: 30px;
       padding: 20px;
+      padding-top: 0;
+      max-width: 750px;
       border-radius: 10px;
-      background: white;
 
       @include desktop {
         margin-bottom: 0;
         padding: 40px;
+        padding-top: 0;
       }
 
       .title {
